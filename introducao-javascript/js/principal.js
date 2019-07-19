d8b7ed39f24f17dbb2c8a2e162faecde952c0716
riff-imaginario <lucas.aleph.101@gmail.com>
2019-07-18 23:23:50 -0300

    add pacientes to table

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -45,4 +45,25 @@ botaoAdicionar.addEventListener('click', function() {
     var peso    = form.peso.value
     var altura  = form.altura.value
     var gordura = form.gordura.value
+
+    var pacienteTr = document.createElement('tr')
+
+    var nomeTd    = document.createElement('td')
+    var pesoTd    = document.createElement('td')
+    var alturaTd  = document.createElement('td')
+    var gorduraTd = document.createElement('td')
+
+    nomeTd.textContent    = nome
+    pesoTd.textContent    = peso
+    alturaTd.textContent  = altura
+    gorduraTd.textContent = gordura
+
+    pacienteTr.appendChild(nomeTd)
+    pacienteTr.appendChild(pesoTd)
+    pacienteTr.appendChild(alturaTd)
+    pacienteTr.appendChild(gorduraTd)
+
+    var tabela = document.querySelector('#tabela-pacientes')
+
+    tabela.appendChild(pacienteTr)  // adiciona paciente à tabela
 })
