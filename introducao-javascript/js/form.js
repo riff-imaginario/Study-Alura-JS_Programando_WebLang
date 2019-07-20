b6a904b0b5a1a583b22d998f5302c622ccdad443
riff-imaginario <lucas.aleph.101@gmail.com>
2019-07-19 23:42:30 -0300

    modify logic to use 'paciente' object

--- introducao-javascript/js/form.js
+++ introducao-javascript/js/form.js
@@ -1,15 +1,11 @@
+// Código acionado ao clicar no botão "Adicionar"
 var botaoAdicionar = document.querySelector('#adicionar-paciente')
+
 botaoAdicionar.addEventListener('click', function() {
     event.preventDefault()  // Previne que página seja recarregada quando o botão for clicado
     
-    var form = document.querySelector('#form-adiciona')  // Seleciona form para pegar dados
-
-    // Pegando valores dos 'names' de cada 'input' no form
-    var nome    = form.nome.value
-    var peso    = form.peso.value
-    var altura  = form.altura.value
-    var gordura = form.gordura.value
-
+    var form       = document.querySelector('#form-adiciona')  // Seleciona form para pegar dados
+    var paciente   = obtemPacienteDoFormulario(form)  // Atribui a paciente, o retorno da função
     var pacienteTr = document.createElement('tr')  // Cria tag table row
 
     // Cria tag table data para cada atributo da tabela 
@@ -20,11 +16,11 @@ botaoAdicionar.addEventListener('click', function() {
     var imcTd     = document.createElement('td')
 
     // Preenche campos que serão usados na tabela
-    nomeTd.textContent    = nome
-    pesoTd.textContent    = peso
-    alturaTd.textContent  = altura
-    gorduraTd.textContent = gordura
-    imcTd.textContent     = calculaIMC(peso, altura)  // Reaproveitamento da função feita em calcula-imc.js
+    nomeTd.textContent    = paciente.nome
+    pesoTd.textContent    = paciente.peso
+    alturaTd.textContent  = paciente.altura
+    gorduraTd.textContent = paciente.gordura
+    imcTd.textContent     = paciente.imc
 
     // Adiciona campos a table row da tabela
     pacienteTr.appendChild(nomeTd)
@@ -37,3 +33,19 @@ botaoAdicionar.addEventListener('click', function() {
 
     tabela.appendChild(pacienteTr)  // adiciona paciente à tabela
 })
+
+
+// Função para obter dados do paciente
+function obtemPacienteDoFormulario(form) {
+
+    // Cria objeto paciente com valores dos 'names' de cada 'input' no form 
+    var paciente = {
+        nome    : form.nome.value,
+        peso    : form.peso.value,
+        altura  : form.altura.value,
+        gordura : form.gordura.value,
+        imc     : calculaIMC(form.peso.value, form.altura.value)  // Reaproveitamento da função feita em calcula-imc.js
+    }
+
+    return paciente
+}
